95328a7f12681bf94c7c05014bd385f1a2c27af7
jjgu <jjgu@ucsc.edu>
2022-01-10 21:25:02 +0000

    first rendition of Lex.c

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -7,6 +7,7 @@
 #define MAX_LEN 300
 
 int main(int argc, char * argv[]){
+	//The beginning part of this program is based on the FileIO.c in the Examples from the class website
 	FILE *in, *out;
 	
 	//check command line for correct number of arguments
@@ -30,62 +31,51 @@ int main(int argc, char * argv[]){
 
 
 	char line[MAX_LEN];
-	chr tokenBuffer[MAX_LEN];
 	int n = 0;
-	int token_count = 0;
-	char* token;
 	int count = 0;
 
+	//count how many lines in the file there are
 	while (fgets(line, MAX_LEN, in) != NULL){
 		n++;
 	}
-
+	//reset the pointer in the infile to the start
 	fseek(in, 0, SEEK_SET);
+	//make the string array using malloc
 	char *arr = (char *)malloc(n * (sizeof(char)));
+	//add each line to the infile, line by line
 	while(fgets(line, MAX_LEN, in) != NULL){
 		char * str;
 		fscanf(in, "%s", str);
 		char *str[count];
 		count++;
 	}
-	
+	//create the list of integers
 	int list[n];
+	//add numbers 0-n to the list
 	for(int i = 0; i < n; i++){
 		list[i] = i;
 	}
+	//use insertion sort algorithm to sort the list based on the string array
+	//Credit for the insertion sort algorithm: Assignment 3 pdf from CSE 13s 
 	for(int i = 1; i < n; i++){
 		int j = i;
 		char * temp = arr[i];
-		while(j > 0 && strcmp(temp, arr[j-1]) < 0){
+		while(j > 0 && (strcmp(temp, arr[j-1]) < 0 || strcmp(temp, arr[j-1]) == 0)){
 			list[j] = list[j-1];
 			j--;
 		}
 		list[j] = i;
 	}
-	/*
-	while (fgets(line, MAX_LEN, in) != NULL){
-		line_count++;
-
-		//get tokens in this line
-		token_count = 0;
-		tokenBuffer[0] = '\0';
-		
-		//get first token
-		token = strok(line, " \n");
-		while(token != NULL){ //we have a token
-			//update token buffer
-			strcat(tokenBuffer, "   ");
-			strcat(tokenBuffer, token);
-			strcat(tokenBuffer, "\n");
-			token_count++;
-
-			//ger next token
-			token = strtok(NULL, " n");
-		}
-
+	
+	//print the array in alphabetical order to the output file
+	for(int i = 0; i < n; i++){
+		fprintf(out, "%s\n", arr[list[i]]); 
 	}
-*/
 
-
-
+	//close files
+	fclose(in);
+	fclose(out);
+	//free the string array
+	free(arr);
+	return(0);
 }
