1cc39998b38bad142dd18f16d0b424802a6bf8eb
jjgu <jjgu@ucsc.edu>
2022-01-18 03:10:31 +0000

    did constructors

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -12,7 +12,7 @@ typedef struct GraphObj{
 	int *distance;
 	int order;
 	int size;
-	int recent;
+	int source;
 }
 
 /*** Constructors-Destructors ***/
@@ -24,36 +24,106 @@ Graph newGraph(int n){
 	G->distance = calloc(n+1, sizeof(int));
 	G->order = n;
 	G->size = 0;
-	G->recent = NIL;
+	G->source = NIL;
 	return G;
 }
 
 void freeGraph(Graph* pG){
-	
+	if(pG != NULL && * pG != NULL){
+		for(int i = 1; i <= getOrder(pG); i++){
+			freeList(&(pG->neighbors[i]));
+		}
+		free((*pG)->neighbors);
+		free((*pG)->color);
+		free((*pG)->parent);
+		free((*pG)->distance);
+		free(*pG);
+		*pG = NULL;
+	}	
 }
 
 /*** Access functions ***/
 int getOrder(Graph G){
-
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getOrder() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	return G->order;
 }
 
 int getSize(Graph G){
-
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getSize() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	return G->size;
 }
 
 int getSource(Graph G){
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getSource() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	if(G->source == NIL){
+		return NIL;
+	}
+	return G->source;
 
 }
 
 int getParent(Graph G, int u){
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getParent() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	if(u < 1 || u > getOrder(G)){
+		fprintf(stderr, "Graph Error: calling getParent() with invalid index. The range of index should be 1 <= u <= getOrder(G)\n");
+		exit(EXIT_FAILURE);
+	}
+	if(getSource(G) == NIL){
+		return NIL;
+	}
+	return G->parent[u];
 
 }
 
 int getDist(Graph G, int u){
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getDist() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	if(u < 1 || u > getOrder(G)){
+		fprintf(stderr, "Graph Error: calling getDist() with invalid index. The range of index should be 1 <= u <= getOrder(G)\n");
+		exit(EXIT_FAILURE);
+	}
+	if(getSource(G) == NIL){
+		return INF;
+	}
+	return G->distance[u];
 
 }
 
 void getPath(List L, Graph G, int u){
+	if(G == NULL){
+		fprintf(stderr, "Graph Error: calling getPath() on NULL Graph reference\n");
+		exit(EXIT_FAILURE);
+	}
+	if(u < 1 || u > getOrder(G)){
+		fprintf(stderr, "Graph Error: calling getPath() with invalid index. The range of index should be 1 <= u <= getOrder(G)\n");
+		exit(EXIT_FAILURE);
+	}
+	if(getSource(G) == NIL){
+		fprintf(stderr, "Graph Error: calling getPath() on NIL Source. BFS() must be called before getPath()\n");
+		exit(EXIT_FAILURE);
+	}
+	if(getSource(G) == u){
+		append(L, u);
+	}else if(G->parent[u] == NIL){
+		append(L, NIL);
+	}else{
+		getPath(L, G, getParent(G, u);
+		append(L, u);
+	}
 
 }
 
