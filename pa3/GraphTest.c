ef14c45610bef37fcf85707726f4b5034181fda4
jjgu <jjgu@ucsc.edu>
2022-01-30 04:15:10 +0000

    testing

--- pa3/GraphTest.c
+++ pa3/GraphTest.c
@@ -9,38 +9,64 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "Graph.h"
 #include "List.h"
+#include "Graph.h"
+
 
 int main (){
-	int n = 5;
-	Graph G = newGraph(n);
-
-	int order = getOrder(G);
-	int size = getSize(G);
-	int source = getSource(G);
-	int parent = getParent(G, 2);
-	int distance = getDist(G, 2);
-       	printf("order: %d\nsize: %d\nsource: %d\nparent: %d\ndistance: %d\n", order, size, source, parent, distance);
-	addEdge(G, 1, 2);
-	addEdge(G, 2, 3);
-	addEdge(G, 3, 4);
-	BFS(G, 1);	
-	order = getOrder(G);
-	size = getSize(G);
-	source = getSource(G);
-	parent = getParent(G, 2);
-	distance = getDist(G, 2);
-       	printf("order: %d\nsize: %d\nsource: %d\nparent: %d\ndistance: %d\n", order, size, source, parent, distance);
-	List L = newList();
-	getPath(L, G, 4);
-	printf("path 1 to 4: ");
-	printList(stdout, L);
-	freeList(&L);
-	printf("\n");
+	Graph G = newGraph(8);
+	addArc(G, 1, 2);
+	addArc(G, 2, 3);
+	addArc(G, 2, 5);
+	addArc(G, 2, 6);
+	addArc(G, 3, 4);
+	addArc(G, 3, 7);
+	addArc(G, 4, 3);
+	addArc(G, 4, 8);
+	addArc(G, 5, 1);
+	addArc(G, 5, 6);
+	addArc(G, 6, 7);
+	addArc(G, 7, 6);
+	addArc(G, 7, 8);
+	addArc(G, 8, 8);
 	printGraph(stdout, G);
-	makeNull(G);
+	Graph GT = transpose(G);
+	printGraph(stdout, GT);
+	Graph CPY = copyGraph(G);
+	printGraph(stdout, CPY);	
+	List S = newList();
+	for(int i = 1; i <= getOrder(G); i++){
+		append(S, i);
+	}
+	printList(stdout, S);
+	fprintf(stdout, "\n");
+	DFS(G, S);
+	printList(stdout, S);
+	fprintf(stdout, "\n");
+	DFS(GT, S);
+	printList(stdout, S);
+	fprintf(stdout, "\n");
+	moveBack(S);
+	for(int i = 1; i <= length(S); i++){
+		printf("curr in stack: %d\n", get(S));
+		if(getParent(GT, get(S)) == NIL){
+			printf("no parent\n");
+		}else{
+			printf("curr parent %d\n", getParent(GT, get(S)));
+		}
+		movePrev(S);
+	}
 	freeGraph(&G);
+	freeGraph(&GT);
+	freeList(&S);
+
+
+
+
+
+
+
+
 
 
 }
