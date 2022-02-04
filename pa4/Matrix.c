c0bb9c77fbe4c23fd2fd2b1fd11e5818ae538e6b
jjgu <jjgu@ucsc.edu>
2022-02-04 08:19:49 +0000

    constructorsand access functions

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -36,18 +36,52 @@ void freeEntry(Entry *pE){
 // newMatrix()
 // Returns a reference to a new nXn Matrix object in the zero state.
 Matrix newMatrix(int n){
-	
+	Matrix M = malloc(sizeof(MatrixObj));
+	M->rows = malloc(sizeof(List) * n);
+	for(int i = 1; i <= n; i++){
+		M->rows[i] = newList();
+	}
+	M->size = n;
+	M->nnz = 0;
+	return(M);	
 }
 // freeMatrix()
 // Frees heap memory associated with *pM, sets *pM to NULL.
-void freeMatrix(Matrix* pM);
+void freeMatrix(Matrix* pM){
+	if(pM != NULL && *pM != NULL){
+		/*
+		makeZero(*pM);
+		for(int i = 0; i < size(*pM); i++){
+			freeList(&((*pM)->rows[i]));
+		}
+		free((*pM)->rows);
+		*/
+		free(*pM);
+		*pM = NULL;
+	}
+	
+}
+
 // Access functions 
 // size()
 // Return the size of square Matrix M.
-int size(Matrix M);
+int size(Matrix M){
+	if(M == NULL){
+		fprintf(stderr, "Matrix Error: calling size() on NULL Matrix reference\n");
+		exit(EXIT_FAILURE);
+	}
+	return M->size;
+}
 // NNZ()
 // Return the number of non-zero elements in M.
-int NNZ(Matrix M);
+int NNZ(Matrix M){
+	if(M == NULL){
+		fprintf(stderr, "Matrix Error: calling NNZ() on NULL Matrix reference\n");
+		exit(EXIT_FAILURE);
+	}
+	return M->nnz;
+}
+
 // equals()
 // Return true (1) if matrices A and B are equal, false (0) otherwise.
 int equals(Matrix A, Matrix B);
