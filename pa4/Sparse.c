64d61998b63c4e249df62015e96f53e85e55db48
James Gu <jjgu@ucsc.edu>
2022-02-05 19:32:30 +0000

    Update Sparse.c

--- pa4/Sparse.c
+++ pa4/Sparse.c
@@ -5,4 +5,26 @@
 #include "List.h"
 #include "Matrix.h"
 
+int main(int argc, char * argv[]){
+	//The beginning part of this program is based on the FileIO.c in the Examples from the class website
+	FILE *in, *out;
+	
+	//check command line for correct number of arguments
+	if(argc != 3){
+		fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	//open files for reading and writing
+	in = fopen(argv[1], "r");
+	if(in == NULL){
+		fprintf(stderr, "Unable to open file %s for reading\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	out = fopen(argv[2], "w");
+	if(out == NULL){
+		fprintf(stderr, "Unable to open file %s for writing\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
 
