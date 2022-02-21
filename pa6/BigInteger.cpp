371f5b873b9f63c531f364e82ffad58c7b176caf
jjgu <jjgu@ucsc.edu>
2022-02-21 23:31:43 +0000

    did up to makeZero

--- pa6/BigInteger.cpp
+++ pa6/BigInteger.cpp
@@ -2,39 +2,83 @@
 // BigInteger.cpp
 // File for the BigInteger ADT
 //-----------------------------------------------------------------------------
-#include<iostream>
-#include<string>
-#include"List.h"
-#include"BigInteger.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include "List.h"
+#include "BigInteger.h"
 
-
-// Exported type  -------------------------------------------------------------
-class BigInteger{
-
-private:
-
-   // BigInteger Fields 
-   int signum;    // +1 (positive), -1 (negative), 0 (zero)
-   List digits;   // List of digits in this BigInteger
-
-public:
+int power = 9;
+long base = 1000000000;
 
    // Class Constructors & Destructors ----------------------------------------
 
    // BigInteger()
    // Constructor that creates a new BigInteger in the zero state: 
    // signum=0, digits=().
-   BigInteger();
+BigInteger::BigInteger(){
+	signum = 0;
+	digits = new List();
+}
 
    // BigInteger()
    // Constructor that creates a new BigInteger from the string s.
    // Pre: s is a non-empty string consisting of (at least one) base 10 digit
    // {0,1,2,3,4,5,6,7,8,9}, and an optional sign {+,-} prefix.
-   BigInteger(std::string s);
+BigInteger::BigInteger(std::string s){
+	if(s.length() == 0){
+		throw std::invalid_argument("BigInteger: Contructor: empty string");
+	}
+	if(s[0] != '+' && s[0] != '-' && !std::isdigit(s[0])){
+		throw std::invalid_argument("BigInteger: Constructor: non-numeric string");
+	}
+	int num_digits = 0;	
+	for(int i = 1; i <= s.length(); i++){
+		num_digits++;
+		if(!std::isdigit(s[i])){
+			throw std::invalid_argument("BigInteger: Constructor: non-numeric string");
+		}
+	}
+	if(s[0] == '-'){
+		signum = -1;
+	}else{
+		signum = 1;
+	}
+	std::string temp_s = "";
+	digits = newList();
+	digits.moveFront();
+	//ceiling of num_digits/power and num_digits/num_entries without math.h
+	int num_entries = (num_digits/power) + ((num_digits % power) != 0);
+	int num_digits_per_entry = (num_digits/num_entries) + ((num_digits % num_entries) != 0);
+	int curr = 0;
+	if(s[0] == '-' || s[0] == '+'){
+		curr+=1;
+	}
+	for(int i = 0; i < num_entries; i++){
+		for(int j = 0; j < num_digits_per_entry; j++){
+			temp_s += s[curr];
+		}
+		long curr_num = atol(temp_s);
+		digits.insertBefore(curr_num);
+		digits.moveNext();
+	}
+	//remove leading zeros
+	digits.moveBack();
+	while(digits.get()==0){
+		digits.deleteBack();
+		digits.moveBack();	
+	}	
+	
+
+	
+}
 
    // BigInteger()
    // Constructor that creates a copy of N.
-   BigInteger(const BigInteger& N);
+BigInteger::BigInteger(const BigInteger& N){
+	this->signum = N.signum;
+	this->digits = N.digits;
+}
 
    // Optional Destuctor
    // ~BigInteger()
@@ -46,24 +90,80 @@ public:
    // sign()
    // Returns -1, 1 or 0 according to whether this BigInteger is positive, 
    // negative or 0, respectively.
-   int sign() const;
+int BigInteger::sign() const{
+	return signum;
+}
 
    // compare()
    // Returns -1, 1 or 0 according to whether this BigInteger is less than N,
    // greater than N or equal to N, respectively.
-   int compare(const BigInteger& N) const;
+int BigInteger::compare(const BigInteger& N) const{
+	if(sign() > N.sign()){
+		return 1;
+	}
+	if(sign() < N.sign()){
+		return -1;
+	}
+	if(sign() == 1 && N.sign() == 1){		
+		if(length(this->digits) > length(N.digits)){
+			return 1;
+		}
+		if(length(this->digits) < length(N.digits)){
+			return -1;
+		}
+	}
+	if(sign() == -1 && N.sign() == -1){		
+		if(length(this->digits) > length(N.digits)){
+			return -1;
+		}
+		if(length(this->digits) < length(N.digits)){
+			return 1;
+		}
+	}
+	if(signum == 0 && N.signum == 0){
+		return 0;
+	}
+	digits.moveFront();
+	N->digits.moveFront();
+	while(digits.index() != -1 && N.digits.index() != -1){
+		if(sign() == 1 && N.sign() == 1){
+			if(digits.get() > N->digits.get()){
+				return 1;
+			}
+			if(digits.get() < N->digits.get()){
+				return -1;
+			}
+		}
+		if(sign() == -1 && N.sign() == -1){
+			if(digits.get() > N->digits.get()){
+				return -1;
+			}
+			if(digits.get() < N->digits.get()){
+				return 1;
+			}
+		}
+		digits.moveNext();
+		N->digits.moveNext();
+	}
+	return 0;
+}
 
 
    // Manipulation procedures -------------------------------------------------
 
    // makeZero()
    // Re-sets this BigInteger to the zero state.
-   void makeZero();
+void BigInteger::makeZero(){
+	digits.clear();
+	signum = 0;
+}
 
    // negate()
    // If this BigInteger is zero, does nothing, otherwise reverses the sign of 
    // this BigInteger positive <--> negative. 
-   void negate();
+void BigInteger::negate(){
+	signum *= -1;
+}
 
 
    // BigInteger Arithmetic operations ----------------------------------------
