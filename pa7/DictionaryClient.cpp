9ca9126701d74375ba78eda96100b9bf38359616
James Gu <jjgu@ucsc.edu>
2022-03-05 05:38:42 +0000

    Update DictionaryClient.cpp

--- pa7/DictionaryClient.cpp
+++ pa7/DictionaryClient.cpp
@@ -1,5 +1,9 @@
 //-----------------------------------------------------------------------------
-// DictionaryClient.cpp
+// 
+// James Gu
+// jjgu
+// pa7
+//DictionaryTest.cpp
 // A test client for the Dictionary ADT
 //-----------------------------------------------------------------------------
 #include<iostream>
@@ -13,7 +17,7 @@ int main(){
 
    string s;
    int x;
-   string S[] = {"one", "two", "foo", "bar", "happy", "is", "three", "par"};
+   string S[] = {"a", "b", "c", "d"};
 
    Dictionary A;
    Dictionary B;
@@ -21,7 +25,7 @@ int main(){
    cout << endl;
 
    // insert some pairs into A
-   for(int i=0; i<8; i++){
+   for(int i=0; i<4; i++){
       A.setValue(S[i], i+1);
    }
    
@@ -31,7 +35,7 @@ int main(){
    cout << "A.size() = " << A.size() << endl  << A << endl;
    cout << "B.size() = " << B.size() << endl  << B << endl;
 
-   B.setValue("bar", 9);
+   B.setValue("b", 9);
 
    // call copy constructor
    Dictionary C = B;
@@ -39,29 +43,17 @@ int main(){
    cout << "B.size() = " << B.size() << endl  << B << endl;
    cout << "C.size() = " << C.size() << endl  << C << endl;
 
-   // check operator==()
-   cout << "A==B is " << (A==B?"true":"false") << endl;
-   cout << "B==C is " << (B==C?"true":"false") << endl;
-   cout << "C==A is " << (C==A?"true":"false") << endl << endl;
-
    // perform alterations on A
-   cout << A.getValue("happy") << endl;
-   A.getValue("happy") *= 10; // change the value associated with "happy"
-   cout << A.getValue("happy") << endl << endl;
-   A.remove("one");
-   A.remove("two");
+   cout << A.getValue("d") << endl;
+   A.getValue("d") *= 10; // change the value associated with "happy"
+   cout << A.getValue("c") << endl << endl;
+   A.remove("a");
+   A.remove("b");
 
    // check state of A
    cout << "A.size() = " << A.size() << endl  << A << endl;
    cout << A.pre_string() << endl;
 
-   // do forward iteration on B
-   for(B.begin(); B.hasCurrent(); B.next()){
-      s = B.currentKey();
-      x = B.currentVal();
-      cout << "("+s+", " << x << ") ";
-   }
-   cout << endl << endl;
 
    // do reverse iteration on C
    for(C.end(); C.hasCurrent(); C.prev()){
@@ -70,46 +62,6 @@ int main(){
       cout << "("+s+", " << x << ") ";
    }
    cout << endl << endl;
-   
-   // check exceptions
-   try{
-      A.getValue("blah");
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      A.remove("blah");
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      A.currentKey();
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      A.currentVal();
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }  
-   try{
-      A.next();
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
-   try{
-      A.prev();
-   }catch(logic_error& e){
-      cout << e.what() << endl;
-      cout << "   continuing without interruption" << endl;
-   }
+ 
 
-   cout << endl;  
-
-   return( EXIT_SUCCESS );
 }
